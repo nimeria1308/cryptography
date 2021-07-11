ebbdd4df8b93d749e7afd1fa1393aa1b331a2301
Simona Dimitrova <nimeria1308@gmail.com>
2021-07-11 21:53:17 +0300

    cleanup in playfair.py

--- playfair.py
+++ playfair.py
@@ -1,6 +1,5 @@
 from itertools import permutations
 from textwrap import wrap
-from tqdm import tqdm
 
 
 def playfair_get_character(coords, key):
@@ -26,9 +25,6 @@ def playfair_get_pair(input_pair, key, encode):
     # extract pair into two separate characters a and b
     a, b = input_pair
 
-    if a == b:
-        raise Exception("Characters must differ")
-
     # move down the column/row if encoding
     # move up the column/row if decoding
     # rectangle rule is the same in both cases
@@ -38,16 +34,17 @@ def playfair_get_pair(input_pair, key, encode):
     a = playfair_get_coordinates(a, key)
     b = playfair_get_coordinates(b, key)
 
+    # handle encoding/decoding with partial keys
     if not a or not b:
         return "??"
 
     # same column
     if a[0] == b[0]:
-        # return characters down the column
+        # return characters down/up the column
         c = (a[0], a[1] + x)
         d = (b[0], b[1] + x)
     elif a[1] == b[1]:
-        # return characters down the row
+        # return characters right/left the row
         c = (a[0] + x, a[1])
         d = (b[0] + x, b[1])
     else:
@@ -64,50 +61,57 @@ def playfair_get_pair(input_pair, key, encode):
 
 
 def playfair(plain_text, key, encode):
-    if len(plain_text) % 2 != 0:
-        raise Exception("Text needs to be in pairs")
-
     # split the text into pairs of characters
-    pairs = wrap(plain_text.upper(), 2)
+    pairs = wrap(plain_text, 2)
 
     # encode / decode
     cypher_text = [playfair_get_pair(pair, key, encode) for pair in pairs]
 
-    # back to a string
+    # join the pairs back into a string
     return "".join(cypher_text)
 
 
-def playfair_try_decript(crypt_text, key):
-    # create a list of all letters
+def playfair_try_decript(crypt_text, key, output_file):
+    # create a list of all letters, except for J
     letters = [chr(x) for x in range(ord('A'), ord('Z') + 1) if x != ord('J')]
     key_letters = "".join(key)
 
+    # filter out all letters that are missing in the key
     missing_letters = [x for x in letters if x not in key_letters]
 
-    all_permutations = list(permutations(missing_letters))
-    results = {}
-
-    for p in tqdm(all_permutations):
-        new_key = "".join(KEY)
+    # iterate over all permutations of the missing letters
+    for p in permutations(missing_letters):
+        new_key = "".join(key)
+        # replace placeholders in key "_" with the letter from the permutation
         for l in p:
             new_key = new_key.replace('_', l, 1)
-        new_key = wrap(new_key, 5)
 
-        decrypted = playfair(crypt_text, new_key, False)
-        if "CRYPTOGRAPHY" in decrypted:
-            results["".join(new_key)] = decrypted
+        # wrap the key as 5-letter rows
+        new_key_wrapped = wrap(new_key, 5)
 
-    return results
+        # decrypt and save to output
+        decrypted = playfair(crypt_text, new_key_wrapped, False)
+        output_file.write("%s %s\n" % (new_key, decrypted))
 
 
-KEY = [
-    "NTWZE",
-    "A_FOS",
-    "DGHIR",
-    "__U_B",
-    "____C",
+KEYS = [
+    [
+        "ZENTW",
+        "_B__U",
+        "OSA_F",
+        "IRDGH",
+        "_C___",
+    ],
+    [
+        "ZENTW",
+        "OSA_F",
+        "IRDGH",
+        "_B__U",
+        "_C___",
+    ],
 ]
 
-c = "EBQXZLHDLKIVQGOMALEBVBDOSGSFZRANDAMOLBSEELSOZLKDCOZFGSIN"
-res = playfair_try_decript(c, KEY)
-print(res)
+cipher_text = "EBQXZLHDLKIVQGOMALEBVBDOSGSFZRANDAMOLBSEELSOZLKDCOZFGSIN"
+with open("playfair_output.txt", "w") as f:
+    for key in KEYS:
+        playfair_try_decript(cipher_text, key, f)
