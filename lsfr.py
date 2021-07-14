e518c801e865c21bc3a4bbc28efe92d462dce9ab
Simona Dimitrova <nimeria1308@gmail.com>
2021-07-14 22:29:20 +0300

    lsfr.py

--- lsfr.py
+++ lsfr.py
@@ -9,7 +9,7 @@ def pop_count(x):
     return c
 
 
-def lsfr(taps, value, size):
+def lsfr(coeffs, value, size):
     # generate mask
     mask = (1 << size) - 1
 
@@ -17,18 +17,18 @@ def lsfr(taps, value, size):
     new_value = (value << 1) & mask
 
     # find new bit
-    new_value |= (pop_count(value & taps) % 2)
+    new_value |= (pop_count(value & coeffs) % 2)
 
     return new_value
 
 
-def find_period(taps, start_value, size):
+def find_period(coeffs, start_value, size):
     current = start_value
     t = 0
     previous = set([start_value])
 
     while True:
-        current = lsfr(taps, current, size)
+        current = lsfr(coeffs, current, size)
         t += 1
 
         if current in previous:
@@ -39,17 +39,17 @@ def find_period(taps, start_value, size):
     return t
 
 
-def find_taps_with_period(size):
+def find_coeffs_with_period(size):
     max_value = (1 << size) - 1
     results = {}
 
-    for taps in range(1, max_value + 1):
+    for coeffs in range(1, max_value + 1):
         found_period = True
-        period = find_period(taps, 1, size)
+        period = find_period(coeffs, 1, size)
 
         if period:
             for start_value in range(2, max_value+1):
-                p = find_period(taps, start_value, size)
+                p = find_period(coeffs, start_value, size)
                 if p != period:
                     found_period = False
                     break
@@ -57,31 +57,30 @@ def find_taps_with_period(size):
             found_period = False
 
         if found_period:
-            results[taps] = period
+            results[coeffs] = period
 
     return results
 
 
-def print_lsfr(taps, start_value, size):
+def print_lsfr(coeffs, start_value, size):
     current = start_value
-    period = find_period(taps, start_value, size)
-    fmt = "period={:d} taps={:0%db}" % size
-    print(fmt.format(period, taps))
+    period = find_period(coeffs, start_value, size)
+    fmt = "period={:d} coeffs={:0%db}" % size
+    print(fmt.format(period, coeffs))
 
     print("t  S")
     fmt = "{:2d} {:0%db}" % size
     for t in range(period + 1):
         print(fmt.format(t, current))
-        current = lsfr(taps, current, size)
+        current = lsfr(coeffs, current, size)
     print("")
 
 
-taps = find_taps_with_period(6)
-for tap, period in taps.items():
-    print("taps={:06b} period={}".format(tap, period))
+all_coeffs = find_coeffs_with_period(6)
+for coeffs, period in all_coeffs.items():
+    print("coeffs={:06b} period={}".format(coeffs, period))
 
-taps_21 = [tap for tap, period in taps.items() if period == 21]
+coeffs_21 = [coeffs for coeffs, period in all_coeffs.items() if period == 21]
 
-for tap in taps_21:
-    print("taps={:06b}".format(tap))
-    print_lsfr(tap, 1, 6)
+for coeffs in coeffs_21:
+    print_lsfr(coeffs, 1, 6)
